a6ee715666faf4dcefab309740a710fd73894e66
Timur <tim.id.campus@gmail.com>
2023-05-18 16:13:38 +0200

    generate UUID and register on the server

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
 function LoginPage() {
     let navigate = useNavigate();
-    let uuid = 'null';
+    const [uuid, setUuid] = useState(null);
+    const [serverResponse, setServerResponse] = useState(null);
 
     const handleLogin = () => {
         navigate('/main');
     }
 
-    const handleRegistration = () => {
-        uuid = uuidv4();
+    const handleRegistration = async () => {
+        const newUuid = uuidv4();
+        setUuid(newUuid);
+
+        const response = await fetch(`http://localhost:8080/user/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ uuid: newUuid })
+        });
+
+        const responseData = await response.json();
+
+        setServerResponse(responseData.message);
     }
 
     return (
@@ -22,7 +36,8 @@ function LoginPage() {
             <button onClick={handleLogin}>Login</button>
             <button onClick={handleRegistration}>Register</button>
             <p><a href="/forgotpassword">Forgot password?</a></p>
-            <div>{uuid}</div>
+            {uuid && <div>{uuid}</div>}
+            {serverResponse && <div>{serverResponse}</div>}
         </div>
     );
 }
