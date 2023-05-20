16bf70d4e1a6a6936f42c5641fcbb4373be4a197
lisamariepleyer <Lisa-Marie Pleyer>
2023-05-20 22:45:02 +0200

    beautify requirements returned from response

--- src/RequirementsTable.js
+++ src/RequirementsTable.js
@@ -2,6 +2,25 @@ import React from 'react';
 
 const RequirementsTable = ({ data }) => {
 
+    /* Requirement is returned packaged in some additional text:
+     "{\r\n    requirement: Some text.\r\n}"
+     This function returns the raw requirement which should be shown to the user.
+     */
+    const extractRequirementText = (requirement) => {
+        try {
+            const startMarker = 'requirement: ';
+            const endMarker = '\r\n}';
+            const startIndex = requirement.indexOf(startMarker);
+            const endIndex = requirement.indexOf(endMarker, startIndex + startMarker.length);
+            if (startIndex !== -1 && endIndex !== -1) {
+                return requirement.substring(startIndex + startMarker.length, endIndex);
+            }
+        } catch (error) {
+            console.error('Error parsing requirement:', error);
+            return 'error';
+        }
+    };
+    
     return (
         <table>
             <thead>
@@ -13,7 +32,7 @@ const RequirementsTable = ({ data }) => {
             <tbody>
             {data.map((item) => (
                 <tr key={item.id}>
-                    <td>{item.requirement}</td>
+                    <td>{extractRequirementText(item.requirement)}</td>
                     <td>{item.ruppScheme ? '✅' : '❌'}</td>
                 </tr>
             ))}
