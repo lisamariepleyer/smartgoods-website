bfd4f1ab3c622c422a0cef5924f79f7275988b96
Timur <timur.idalov@stud.fh-campuswien.ac.at>
2023-05-21 13:11:30 +0200

    Bring back Close button.

--- src/Modal.js
+++ src/Modal.js
@@ -6,6 +6,7 @@ function Modal({ open, onClose, children }) {
     return (
         <div style = {{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: '#fff', padding: '50px', zIndex: 1000 }}>
             {children}
+            <button onClick={onClose}>Close</button>
         </div>
     )
 }
