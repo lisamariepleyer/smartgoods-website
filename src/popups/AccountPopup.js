014e296298b2d484626f8ab474f5a610d4da8e7d
lisamariepleyer <Lisa-Marie Pleyer>
2023-06-25 17:10:27 +0200

    implement logout button

--- src/popups/AccountPopup.js
+++ src/popups/AccountPopup.js
@@ -3,9 +3,28 @@ import Modal from '../popups/Modal';
 import {Button} from "../components/Button";
 
 import { UserContext } from '../common/UserContext';
+import {useNavigate} from "react-router-dom";
 
 function AccountPopup({ open, onClose }) {
-    const { currentUser } = useContext(UserContext);
+    let navigate = useNavigate();
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+
+    const handleLogout = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/api/v2/users/' + currentUser.username + '/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+
+            setCurrentUser(null);
+            navigate('/');
+
+        } catch (error) {
+            console.error('Failed to log out:', error);
+        }
+    };
 
     return (
         <Modal open={open} onClose={onClose} popupName="Account Info">
@@ -13,6 +32,10 @@ function AccountPopup({ open, onClose }) {
                 <h3>Username</h3>{currentUser.username}
                 <h3>First Name</h3>{currentUser.firstName}
                 <h3>Last Name</h3>{currentUser.lastName}
+                <br/>
+                <br/>
+                <br/>
+                <Button onClick={handleLogout}>Logout</Button><br/>
             </div>
         </Modal>
     )
